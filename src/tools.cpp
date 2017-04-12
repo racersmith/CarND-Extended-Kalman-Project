cc9c10132b464c7d2c048ba0cf2cc0e180779772
Josh Smith <racersmith@gmail.com>
2017-04-12 05:46:21 -0700

    add CalculateRMSE add function comments add vs project files to .gitignore

--- src/tools.cpp
+++ src/tools.cpp
@@ -9,6 +9,8 @@ Tools::Tools() {}
 
 Tools::~Tools() {}
 
+// Calculate the RMSE error over the test data for model evaluation.
+// Returns a vector of resulting errors
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
   /**
@@ -40,11 +42,34 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 	}
 }
 
+// Takes the current state vector and calculates the Jacobian
+// matrix for use in the sensor update for radar.
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
+	// Jacobian
+	MatrixXd Hj(3, 4);
+
+	// Pull parameters out of state
+	float px = x_state(0);
+	float py = x_state(1);
+	float vx = x_state(2);
+	float vy = x_state(3);
+
+	// Start pre-compute of repeating denominators
+	float x2y2 = px*px + py*py;      // x^2+y^2
 
 	// Check px*px + py*py == 0, set to small value to avoid divZero errors
+	if (fabs(x2y2) < 0.0001) {
+		x2y2 = 0.0001;
+	}
+
+	// Finish pre-compute
+	float x2y2_05 = sqrt(x2y2);      // (x^2+y^2)^0.5
+	float x2y2_15 = x2y2 * x2y2_05;  // (x^+y^2)^1.5
+
+	// Compute the Jacobian matrix
+	Hj << (px / x2y2_05), (py / x2y2_05), 0, 0,
+		-(py / x2y2), (px / x2y2), 0, 0,
+		(py*(vx*py - vy*px) / x2y2_15), (px*(px*vy - py*vx) / x2y2_15), (px / x2y2_05), (py / x2y2_05);
+
+	return Hj;
 }
